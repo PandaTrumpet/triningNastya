0fb4a622daf8fa23e6c15f120080df4dbf731397
PandaTrumpet <dsavontrumpet@gmail.com>
2024-02-10 22:23:21 +0200

    update

--- src/main.js
+++ src/main.js
@@ -5,6 +5,46 @@ import SimpleLightbox from "simplelightbox";
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
 
-console.log(SimpleLightbox);
+
+
+
+
+// const gallery = document.querySelector('.guliver-menu');
+
+// const link = document.querySelector('.img-guliver');
+
+
+
+
+// function createMarkup({ original, preview, description }) {
+//   const markup = ` <li class="img-guliver">
+//     <a class="big"  href="${original}">
+//       <img
+//         class="gallery-image"
+//         src="${preview}"
+//         alt="${description}"
+//       />
+//     </a>
+//   </li>`;
+//   return markup;
+// }
+
+// let paste = '';
+
+// for (let image of images) {
+//   paste += createMarkup(image);
+// }
+// gallery.innerHTML = paste;
+
+const lightbox = new SimpleLightbox('.guliver-menu a', {
+    captionsData: "alt",
+    captionDelay:250,
+});
+
+
+
+const lightbox2 = new SimpleLightbox('.gurden-menu a', {
+    captionsData: "alt",
+    captionDelay:250,
+});
